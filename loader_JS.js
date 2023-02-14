85bedc3250ebc319747796fa1c4c4b44ce8af7a7
Awab Ibrahim Ishag Eltahir <125273396+Awab-IsaaQ@users.noreply.github.com>
2023-02-14 11:00:38 +0200

    Add files via upload

---
+++ loader_JS.js
@@ -0,0 +1,18 @@
+const loader = document.querySelector(".loader");
+const main = document.querySelector(".main");
+
+
+function init(){
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+
+        main.style.display = 'block';
+        setTimeout(() => {
+            main.style.opacity = 1;
+        }, 20)
+        
+    }, 1500)
+}
+
+init();
